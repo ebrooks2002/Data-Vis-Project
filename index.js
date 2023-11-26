bbd5f353b5c4a741d82d8322642e29a0e894538f
ebrooks2002 <84427247+ebrooks2002@users.noreply.github.com>
2023-11-25 21:51:35 -0500

    commit

--- index.js
+++ index.js
@@ -1,15 +1,120 @@
 
-var nytdata = d3.csv("nyt-metadata-sample.csv");
-var metadata = d3.csvParse(nytdata.text());
-var h = d3.extent(metadata, d => d.pub_date_year)
-var years = h.map(Number)
-var dataInitial = metaData.filter(d => d.pub_date_year == yearFilter)
+var myFullpage = new fullpage('#fullpage', {
+  // Navigation
+  menu: '#menu',
+  lockAnchors: false,
+  anchors:['firstPage', 'secondPage'],
+  navigation: false,
+  navigationPosition: 'right',
+  navigationTooltips: ['firstSlide', 'secondSlide'],
+  showActiveTooltip: false,
+  slidesNavigation: false,
+  slidesNavPosition: 'bottom',
+
+  // Scrolling
+  css3: true,
+  scrollingSpeed: 600,
+  autoScrolling: false,
+  fitToSection: true,
+  fitToSectionDelay: 600,
+  scrollBar: false,
+  easing: 'easeInOutCubic',
+  easingcss3: 'ease',
+  loopBottom: false,
+  loopTop: false,
+  loopHorizontal: true,
+  continuousVertical: false,
+  continuousHorizontal: false,
+  scrollHorizontally: false,
+  interlockedSlides: false,
+  dragAndMove: false,
+  offsetSections: false,
+  resetSliders: false,
+  fadingEffect: false,
+  normalScrollElements: '#element1, .element2',
+  scrollOverflow: true,
+  scrollOverflowMacStyle: false,
+  scrollOverflowReset: false,
+  touchSensitivity: 15,
+  bigSectionsDestination: null,
+
+  // Accessibility
+  keyboardScrolling: true,
+  animateAnchor: true,
+  recordHistory: true,
+
+  //License Key
+  //licenseKey: null,
+
+  // Design
+  controlArrows: true,
+  controlArrowsHTML: [
+      '<div class="fp-arrow"></div>', 
+      '<div class="fp-arrow"></div>'
+  ],
+  verticalCentered: false,
+  sectionsColor : ['#ccc', '#fff'],
+  paddingTop: '3em',
+  paddingBottom: '10px',
+  fixedElements: '#header, .footer',
+  responsiveWidth: 0,
+  responsiveHeight: 0,
+  responsiveSlides: false,
+  parallax: false,
+  parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+  dropEffect: false,
+  dropEffectOptions: { speed: 2300, color: '#F82F4D', zIndex: 9999},
+  waterEffect: false,
+  waterEffectOptions: { animateContent: true, animateOnMouseMove: true},
+  cards: false,
+  cardsOptions: {perspective: 100, fadeContent: true, fadeBackground: true},
+
+  // Custom selectors
+  sectionSelector: '.section',
+  slideSelector: '.slide',
+
+  lazyLoading: true,
+  observer: true,
+  credits: { enabled: true, label: 'Made with fullPage.js', position: 'right'},
+
+  // Events
+  beforeLeave: function(origin, destination, direction, trigger){},
+  onLeave: function(origin, destination, direction, trigger){},
+  afterLoad: function(origin, destination, direction, trigger){},
+  afterRender: function(){},
+  afterResize: function(width, height){},
+  afterReBuild: function(){},
+  afterResponsive: function(isResponsive){},
+  afterSlideLoad: function(section, origin, destination, direction, trigger){},
+  onSlideLeave: function(section, origin, destination, direction, trigger){},
+  onScrollOverflow: function(section, slide, position, direction){}
+});
+var data;
+var h;
+var years;
+var dataInitial;
+var articleCounts;
+var articleCountsArray
+
+d3.text("nyt-metadata-sample.csv").then(function(csvString) {
+  // 'csvString' contains the raw CSV file content
+  // If you want to parse it into objects after some preprocessing:
+  data = d3.csvParse(csvString);
+  h = d3.extent(data, d => d.pub_date_year);
+  years = h.map(Number);
+  dataInitial = data.filter(d => d.pub_date_year == yearFilter);
+  articleCounts = d3.rollup(dataInitial, v => v.length, d => d.section_name);
+  articleCountsArray = Array.from(articleCounts, ([section_name, count]) => ({ section_name, count}));
+  console.log(data);
+}).catch(function(error) {
+  // Handle any loading errors here
+  console.error(error);
+});
+
 const height = 600
 const width = 900
 const margin = ({top: 10, right: 40, bottom:75, left: 40})
-var articleCounts = d3.rollup(dataInitial, v => v.length, d => d.section_name);
-var articleCountsArray = Array.from(articleCounts, ([section_name, count]) => ({ section_name, count}));
-console.log(articleCounts)
+
 var x = d3.scaleBand()
     .domain(dataInitial.map(d => d.section_name)) // Assuming 'category' is the categorical field
     .range([0, width - margin.left - margin.right])
